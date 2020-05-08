540aac1ed7e704bcbfc509f2fee4ed38ff816b12
Derry Cheng <derryc09@uw.edu>
2020-05-08 07:32:35 -0700

    Check mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header.js';
+import Content from './Components/Content/Content';
 function App() {
   return (
     <div>
       <Header />
-    
+      <Content />
     </div>
   );
 }
