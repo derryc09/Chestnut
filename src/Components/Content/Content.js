758a96418a84195e29a3814dc887cf9b3836a308
Derry Cheng <derryc09@uw.edu>
2020-05-08 07:43:45 -0700

    Test deploy

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -3,7 +3,7 @@ class Content extends React.Component{
     render(){
         return (
             <div className="container">
-                Hello 紹文弟弟
+                Hello 紹文弟弟 HII
             </div>
         );
     }
