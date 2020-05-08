53547050b24d0ad446cd8f86c1beae71067150b1
Derry Cheng <derryc09@uw.edu>
2020-05-08 07:23:20 -0700

    Updates

---
+++ precache-manifest.ffd02fe37be44e1be8e2a98dd07eb873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227085cee5ebca39c4c4ec3d720caf4e",
+    "url": "/Chestnut/index.html"
+  },
+  {
+    "revision": "ae66770e3ff9e4932d31",
+    "url": "/Chestnut/static/css/2.ef1efacd.chunk.css"
+  },
+  {
+    "revision": "c04ebf10e9c0071c1f50",
+    "url": "/Chestnut/static/css/main.63db4ee4.chunk.css"
+  },
+  {
+    "revision": "ae66770e3ff9e4932d31",
+    "url": "/Chestnut/static/js/2.16b970cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chestnut/static/js/2.16b970cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04ebf10e9c0071c1f50",
+    "url": "/Chestnut/static/js/main.9d246ef0.chunk.js"
+  },
+  {
+    "revision": "0e63cd774d476dcfd103",
+    "url": "/Chestnut/static/js/runtime-main.f5fb25ff.js"
+  }
+]);
